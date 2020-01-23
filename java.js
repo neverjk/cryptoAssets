63aaedd6ad35c4aa1cdfad820e9a5cad6f120448
evdokim <evdokim@gmail.com>
2020-01-23 19:54:14 +0200

    Implement data log

---
+++ java.js
@@ -0,0 +1,10 @@
+﻿$.get('https://poloniex.com/public?command=returnCurrencies', function (data) {
+    console.log(data);
+    var cryptoAssets = jQuery.parseJSON(JSON.stringify(data));
+    console.log(cryptoAssets);
+    var result = [];
+    for (var i in cryptoAssets)
+        result.push([i, cryptoAssets[i]]);
+
+    console.log(result);
+});
